4b8cac65538131fe52c43c145653ac83d37f36d2
Elijah Noble <elijahnobledev@gmail.com>
2022-06-05 13:01:27 -0700

    first commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleFocus(event) {
+        event.preventDefault();
+        console.log("Good!");
+    }
+
+    function handleBlur(event) {
+        event.preventDefault();
+        console.log("Hey! Eyes on me!");
+    }
+
+
+    return (
+       <div>
+            <form onFocus={handleFocus} onBlur={handleBlur}>           
+              <button>Eyes on me</button>
+            </form>
+       </div> 
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function handleChange(event) {
+        event.preventDefault();
+        console.log("Entering password...");
+    }
+
+
+    return (
+       <div>
+            <form onChange={handleChange}>           
+                <input type="password" />
+            </form>
+       </div> 
+    )
+}
+
+export default Keypad;
